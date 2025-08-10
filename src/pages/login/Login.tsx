48bc432c4f5d0ac7ffcd04526143647bf51069ef
Barathi S <19barathi@gmail.com>
2025-08-10 20:38:58 +0530

    error response changed

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -28,7 +28,7 @@ function Login() {
       toast.success("Logged In successfully");
     } catch (error) {
       if (error instanceof AxiosError) {
-        toast.error(error.response?.data.message || error.message);
+        toast.error(error.response?.data.error || error.message);
       } else {
         console.error(error);
         toast.error("Something went wrong");
